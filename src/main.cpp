77567a0984fd84529c43738f08ab3febc17dfe23
cinatit <cinatit.igor.santos@gmail.com>
2023-08-05 13:44:26 -0300

    Extract song box to song-box.xml

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <gtkmm.h>
+#include <vector>
+#include "song.cpp"
 
 int main(int argc, char* argv[]) {
     auto app = Gtk::Application::create(argc, argv);
@@ -11,10 +13,22 @@ int main(int argc, char* argv[]) {
     // Apply the CSS styles to the default screen
     auto screen = Gdk::Screen::get_default();
     auto styleContext = Gtk::StyleContext::create();
-    styleContext->add_provider_for_screen(screen, cssProvider, GTK_STYLE_PROVIDER_PRIORITY_USER);    
+    styleContext->add_provider_for_screen(screen, cssProvider, GTK_STYLE_PROVIDER_PRIORITY_USER);
 
+    // Get window
     Gtk::Window* window;
     builder->get_widget("window", window);
+
+    // Get song-list from the builder
+    Gtk::ListBox* song_list;
+    builder->get_widget("song-list", song_list);
+
+    // Create song box
+    Gtk::Box* song_box;
+    auto song_builder = Gtk::Builder::create_from_file("song-box.xml");
+    song_builder->get_widget("song-box", song_box);
+
+    song_list->append(*song_box);
 
     app->run(*window);
 }
