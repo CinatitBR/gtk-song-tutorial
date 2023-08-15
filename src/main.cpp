b2f1d56e5256197b69b6b46003761499262bedaf
cinatit <cinatit.igor.santos@gmail.com>
2023-08-15 16:54:12 -0300

    Update song_count label widget

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 int main(int argc, char* argv[]) {
     auto app = Gtk::Application::create(argc, argv);
     auto builder = Gtk::Builder::create_from_file("gui.glade");
+    int like_count = 0;
 
     // Load the CSS file
     auto cssProvider = Gtk::CssProvider::create();
@@ -24,6 +25,10 @@ int main(int argc, char* argv[]) {
     Gtk::ListBox* song_list;
     builder->get_widget("song-list", song_list);
 
+    // Get song_count label
+    Gtk::Label* song_count_label;
+    builder->get_widget("song-count", song_count_label);
+
     // Vector containing the song data
     std::vector<Song> songs = Song::read_from_csv("songs.csv");
 
@@ -35,18 +40,21 @@ int main(int argc, char* argv[]) {
         SongItem* song_item;
         song_builder->get_widget_derived("song-box", song_item, &song);
 
-        song_item->signal_like_pressed().connect([](SongItem* song_item_target) -> void {
+        song_item->signal_like_pressed().connect([&like_count, song_count_label](SongItem* song_item_target) -> void {
             std::string heart_solid = "./images/icons/heart-solid-icon.png";
             std::string heart = "./images/icons/heart-icon.png";
 
             if (song_item_target->data->is_liked) {
                 song_item_target->like_icon->set(heart);
+                like_count--;
             }
             else {
                 song_item_target->like_icon->set(heart_solid);
+                like_count++;
             }
 
             song_item_target->data->is_liked = !song_item_target->data->is_liked;
+            song_count_label->set_label(std::to_string(like_count) + " songs");
         });
 
         // Append song box to the list
